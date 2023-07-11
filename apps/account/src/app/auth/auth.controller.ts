f7808832e3d98072cb26dc8659f64141d6e4f8f9
Lev <nefedov.lewa@gmail.com>
2023-07-11 19:25:41 +0400

    fix: move dto classes up

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,21 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from './auth.service';
 
-@Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post('register')
-  async register(@Body() dto: RegisterDto) {
-    return this.authService.register(dto);
-  }
-
-  @Post('login')
-  async login(@Body() { email, password }: LoginDto) {
-    const { id } = await this.authService.validateUser(email, password);
-    return this.authService.login(id);
-  }
-}
 
 export class RegisterDto {
   email: string;
@@ -26,4 +11,21 @@ export class RegisterDto {
 export class LoginDto {
   email: string;
   password: string;
+}
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {
+  }
+
+  @Post('register')
+  async register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
+
+  @Post('login')
+  async login(@Body() {email, password}: LoginDto) {
+    const {id} = await this.authService.validateUser(email, password);
+    return this.authService.login(id);
+  }
 }
